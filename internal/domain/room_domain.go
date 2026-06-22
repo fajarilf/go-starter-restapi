f06b02d061e2a26f8382f9cdf9e8916c1960ef79
fajarilf <fjr.ilf.firmansyah@gmail.com>
2026-06-22 13:38:53 +0700

    update(domain): allow null value in domain.Room updated_at

--- internal/domain/room_domain.go
+++ internal/domain/room_domain.go
@@ -1,13 +1,16 @@
 package domain
 
-import "time"
+import (
+	"database/sql"
+	"time"
+)
 
 type Room struct {
-	Id          int       `db:"id"`
-	Name        string    `db:"name"`
-	Description string    `db:"description"`
-	CreatedAt   time.Time `db:"created_at"`
-	UpdatedAt   time.Time `db:"updated_at"`
+	Id          int          `db:"id"`
+	Name        string       `db:"name"`
+	Description string       `db:"description"`
+	CreatedAt   time.Time    `db:"created_at"`
+	UpdatedAt   sql.NullTime `db:"updated_at"`
 }
 
 type RoomDto struct {
